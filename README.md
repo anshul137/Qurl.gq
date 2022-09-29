360a14d59b310ebfaa6077fb877acdf6eadaff78
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2022-09-28 22:20:29 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Express.js-Boilerplate
+
+Uses `nodemon` and `ejs`
+
+- Ejs templates under views/
+- CSS & JS under public/
+
+# Setup
+
+Install dependencies
+```bash
+npm install
+```
+
+Run server
+```bash
+npm start
+````
---
+++ index.js
@@ -0,0 +1,17 @@
+if (process.env["DEV"]) {
+    require("dotenv").config();
+}
+
+const express = require("express");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(8080, () => {
+    console.log("Listening on port 8080\nhttp://localhost:8080");
+});
