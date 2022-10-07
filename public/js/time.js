1eafad43fc91ba3cb8aabae08e74fe9c8f09ba13
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2022-10-07 13:14:10 -0400

    fix: rename variable

--- public/js/time.js
+++ public/js/time.js
@@ -1,3 +1,4 @@
-for (const time of document.querySelectorAll("[time]")) {
-  time.innerText = new Date(parseInt(time.getAttribute("time"))).toLocaleString()
+for (const timeElement of document.querySelectorAll("[time]")) {
+  const timestamp = parseInt(timeElement.getAttribute("time"));
+  timeElement.innerText = new Date(timestamp).toLocaleString();
 }
