ffa7ae2ef6522e8df9f069dcc0907bc01fea89a4
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2022-10-08 12:22:06 -0400

    fix: update endpoint

--- router/404.js
+++ router/404.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.get("/404", (req, res) => {
+router.get("/", (req, res) => {
     res.status(404);
 
     if (req.accepts('html'))
