755e258f5f46ae1780a8afc74b90b5913cb33c62
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2022-10-08 12:21:11 -0400

    feat: create 404 router

---
+++ router/404.js
@@ -0,0 +1,15 @@
+const router = Router();
+
+router.get("/404", (req, res) => {
+    res.status(404);
+
+    if (req.accepts('html'))
+        return res.render("404");
+
+    if (req.accepts('json'))
+        return res.json({ error: 'Not found' });
+
+    return res.type('txt').send('Not found');
+});
+
+module.exports = router;
--- router/index.js
+++ router/index.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 
+router.use("/404", require("./404"));
 router.use(require("./shortUrl"));
 router.use("/createUrl", require("./createUrl"));
 
@@ -8,21 +9,6 @@ router.get("/", (req, res) => {
     res.render("index");
 });
 
-router.get("/404", (req, res) => {
-  res.status(404);
-
-  if (req.accepts('html')) {
-    res.render("404");
-  }
-
-  else if (req.accepts('json')) {
-    res.json({ error: 'Not found' });
-  }
-
-  else {
-    res.type('txt').send('Not found');
-  }
-});
 
 router.use((req, res) => {
   res.redirect("/404");
