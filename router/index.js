a467de2fa7f0bcd4dfaccf42aca016465735cf41
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2022-10-08 12:17:27 -0400

    fix: create /404 endpoint

--- router/index.js
+++ router/index.js
@@ -8,8 +8,24 @@ router.get("/", (req, res) => {
     res.render("index");
 });
 
+router.get("/404", (req, res) => {
+  res.status(404);
+
+  if (req.accepts('html')) {
+    res.render("404");
+  }
+
+  else if (req.accepts('json')) {
+    res.json({ error: 'Not found' });
+  }
+
+  else {
+    res.type('txt').send('Not found');
+  }
+});
+
 router.use((req, res) => {
-  res.status(404).render("404");
+  res.redirect("/404");
 });
 
 module.exports = router;
